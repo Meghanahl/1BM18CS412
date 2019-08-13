72ea9dbc4cd5273f5ee742eac6ad9b8d3720fb1b
Meghanahl <53852111+Meghanahl@users.noreply.github.com>
2019-08-13 15:54:12 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,12 @@
+def Fibonacci(n): 
+    n1=0
+    n2=1
+    for i in range(n):
+        print(n1)
+        nxt=n1+n2
+        n1=n2
+        n2=nxt
+def main():
+    n=int(input("enter a number"))
+    Fibonacci(n)
+main()
